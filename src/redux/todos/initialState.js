afd58619e67785724c38644683ed8fa71a9ee38a
Abdul Owhab <owhab.cse@gmail.com>
2023-03-07 01:15:25 +0600

    created initialState for todos

---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,19 @@
+export const initialState = [
+    {
+        id: 1,
+        text: "Learn React JS",
+        completed: true,
+    },
+    {
+        id: 2,
+        text: "Learn React Redux",
+        completed: false,
+        color: 'red'
+    },
+    {
+        id: 3,
+        text: "Learn PHP Laravel",
+        completed: false,
+        color: 'skyblue'
+    }
+]
