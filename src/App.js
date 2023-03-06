1657b32b04bd225b574a602f1eb3e10277b55381
Abdul Owhab <owhab.cse@gmail.com>
2023-03-06 22:48:23 +0600

    created TodoList, ToDo and Footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -23,28 +24,8 @@ function App() {
 
                 <hr className="mt-4"/>
 
-                {/*// <!-- footer -->*/}
-                <div className="mt-4 flex justify-between text-xs text-gray-500">
-                    <p>2 tasks left</p>
-                    <ul className="flex space-x-1 items-center text-xs">
-                        <li className="cursor-pointer font-bold">All</li>
-                        <li>|</li>
-                        <li className="cursor-pointer">Incomplete</li>
-                        <li>|</li>
-                        <li className="cursor-pointer">Complete</li>
-                        <li></li>
-                        <li></li>
-                        <li
-                            className="h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer bg-green-500"
-                        ></li>
-                        <li
-                            className="h-3 w-3 border-2 border-red-500 md:hover:bg-red-500 rounded-full cursor-pointer"
-                        ></li>
-                        <li
-                            className="h-3 w-3 border-2 border-yellow-500 md:hover:bg-yellow-500 rounded-full cursor-pointer"
-                        ></li>
-                    </ul>
-                </div>
+                <Footer/>
+
             </div>
         </div>
     </div>
