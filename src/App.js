515af265d2ac31f1a16ae1be47a4d576b5e3e535
Abdul Owhab <owhab.cse@gmail.com>
2023-03-06 21:56:20 +0600

    created Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -12,39 +13,8 @@ function App() {
 
 
             <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
-                {/*// <!-- header -->*/}
-                <div>
-                    <form
-                        className="flex items-center bg-gray-100 px-4 py-4 rounded-md"
-                    >
-                        <img
-                            src="./images/notes.png"
-                            className="w-6 h-6"
-                            alt="Add todo"
-                        />
-                        <input
-                            type="text"
-                            placeholder="Type your todo"
-                            className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
-                        />
-                        <button
-                            type="submit"
-                            className="appearance-none w-8 h-8 bg-[url('./images/plus.png')] bg-no-repeat bg-contain"
-                        ></button>
-                    </form>
 
-                    <ul className="flex justify-between my-4 text-xs text-gray-500">
-                        <li className="flex space-x-1 cursor-pointer">
-                            <img
-                                className="w-4 h-4"
-                                src="./images/double-tick.png"
-                                alt="Complete"
-                            />
-                            <span>Complete All Tasks</span>
-                        </li>
-                        <li className="cursor-pointer">Clear completed</li>
-                    </ul>
-                </div>
+                <Header/>
                 <hr className="mt-4"/>
 
                 {/*// <!-- todo list -->*/}
