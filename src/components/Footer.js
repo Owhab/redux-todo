16cd2524a1f6d0dd41cd376ffc702306011bee30
Abdul Owhab <owhab.cse@gmail.com>
2023-03-11 01:26:36 +0600

    conditionally changing the style of filter status

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {statusChanged} from "../redux/filters/action";
 
 const numberOfTodos = (no_of_todos) => {
     switch (no_of_todos) {
@@ -14,18 +15,27 @@ const numberOfTodos = (no_of_todos) => {
 }
 
 const Footer = () => {
-    const todos = useSelector((state)=> state.todos);
+    const todos = useSelector((state)=> state.todos)
+    const filters = useSelector((state) => state.filters)
     const taskRemaining = todos.filter(todo => !todo.completed).length;
+    const dispatch = useDispatch();
+
+    const {status, colors} = filters;
+
+
+    const handleStatusChanged = (status) => {
+        dispatch(statusChanged(status));
+    }
 
     return (
         <div className="mt-4 flex justify-between text-xs text-gray-500">
             <p>{numberOfTodos(taskRemaining)}</p>
             <ul className="flex space-x-1 items-center text-xs">
-                <li className="cursor-pointer font-bold">All</li>
+                <li className={`cursor-pointer ${status === 'All' &&  'font-bold'} `} onClick={()=>handleStatusChanged('All')}>All</li>
                 <li>|</li>
-                <li className="cursor-pointer">Incomplete</li>
+                <li className={`cursor-pointer ${status === 'Incomplete' && 'font-bold'}  `} onClick={()=>handleStatusChanged('Incomplete')}>Incomplete</li>
                 <li>|</li>
-                <li className="cursor-pointer">Complete</li>
+                <li  className={`cursor-pointer ${status === 'Complete' &&  'font-bold'} `} onClick={()=>handleStatusChanged('Complete')}>Complete</li>
                 <li></li>
                 <li></li>
                 <li
