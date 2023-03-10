82f8d78b50a1cf162e92a2ddfc57d52bb768e866
Abdul Owhab <owhab.cse@gmail.com>
2023-03-11 02:28:12 +0600

    code refactored for filtering

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,7 +6,26 @@ const TodoList = () => {
 
     const todos = useSelector((state) => state.todos);
     // console.log(todos)
-    const filters = useSelector((state) => state.filters)
+    const filters = useSelector((state) => state.filters);
+    const filterByStatus = todo => {
+        const {status} = filters;
+        switch (status){
+            case 'Complete':
+                return todo.completed;
+            case 'Incomplete':
+                return !todo.completed;
+            default:
+                return true;
+        }
+    }
+
+    const filterByColor = todo => {
+        const {colors} = filters;
+        if(colors.length > 0){
+            return colors.includes(todo?.color);
+        }
+        return true;
+    }
 
     return (
         <div
@@ -14,24 +33,8 @@ const TodoList = () => {
         >
             {
                 todos.
-                filter(todo => {
-                    const {status} = filters;
-                    switch (status){
-                        case 'Complete':
-                            return todo.completed;
-                        case 'Incomplete':
-                            return !todo.completed;
-                        default:
-                            return true;
-                    }
-                })
-                .filter(todo => {
-                    const {colors} = filters;
-                    if(colors.length > 0){
-                        return colors.includes(todo?.color);
-                    }
-                    return true;
-                })
+                filter(filterByStatus)
+                .filter(filterByColor)
                 .map(todo => (
                     <ToDo todo = {todo} key = {todo.id}/>
                 ))
